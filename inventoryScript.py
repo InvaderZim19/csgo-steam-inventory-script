7368036ceb4215862dc0d37f69b67d3b30272738
Karl Cassel <thee1gameboy@yahoo.com>
2016-06-15 21:17:23 -0700

    made some comments for what each of the lines do.

--- inventoryScript.py
+++ inventoryScript.py
@@ -8,6 +8,7 @@ import time
 def createInventoryFile(steamid, botNumber):
 	print('Writing file...')
 
+	# this is the json query, it reads each of the items from the inventory
 	data = urllib2.urlopen('http://steamcommunity.com/profiles/'+steamid+'/inventory/json/730/2')
 	json_data = json.loads(data.read())
 	descriptions = json_data['rgDescriptions']
@@ -15,21 +16,24 @@ def createInventoryFile(steamid, botNumber):
 	for v in descriptions:
 		items.append([descriptions[v]['name'], descriptions[v]['descriptions']])
 	
-	# enter items here
+	# enter items here, in lowercase
 	itemSearch = ['device', 'katowice2014']
 
 	findItems('bot' + botNumber + '.txt', items, itemSearch)
 
-	# time.sleep(cycleTime)
-
 def findItems(file, items, searchItems):
 	print('Finding items...')
+
+	# creating current time
 	now = datetime.datetime.now()
 
+	# opening the file containing all the items matched
 	file = open("bot1.txt", 'w')
 
 	matches = []
 
+	# for each item, if any of the items contain the items listed
+	# then print it onto the file
 	for s in searchItems:
 		matches.append([i for i in items if s in str(i)])
 	
@@ -46,6 +50,8 @@ def findItems(file, items, searchItems):
 	print ('Finished! ' + str(datetime.datetime.now()))
 
 
+# this actually runs it, the first perameter changes the ID that you want
+# the second perameter just leave it.
 createInventoryFile('76561197998202248', '1')
 
 
